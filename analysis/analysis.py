891dc2aaaed1db25703be2aa733c795e1c2e8e0e
Daniel Sharp <dannys4@mit.edu>
2025-04-13 16:02:30 -0400

    Working analysis tool

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,26 +1,53 @@
 #!/usr/bin/env python
 import sys
 import os
-from typing import TypedDict, Dict
+from typing import TypedDict, Dict, List
 import json
+import re
 
 class ReceiptItem(TypedDict):
     name: str
-    quantity: int
+    quantity: float
     price: float
 
 def create_receipt_item(itemDict: Dict):
     name: str = itemDict['name']
-    quantityStr: str = itemDict['quantity']
+    quantityMatch = re.search(r'\d+\.?\d{,2}', itemDict['quantity'])
+    if quantityMatch is None:
+        print(itemDict['quantity'])
+        raise ValueError("Could not find quantity!")
+    quantityStr = quantityMatch.group()
     priceStr: str = itemDict['price']
-    quantity = int(quantityStr.split()[-1])
+    quantity = float(quantityStr)
     price = float(priceStr[1:])
-    return ReceiptItem(name, quantity, price)
+    return ReceiptItem(name=name, quantity=quantity, price=price)
 
 def create_receipt(jsonFilename: str):
     with open(jsonFilename, 'r') as f:
         list_json = json.load(f)
-    print(list_json)
+    receipt = [create_receipt_item(it) for it in list_json]
+    return receipt
+
+def find_date(date_string: str, sep: str):
+    date_pattern = r'\d{2}'+sep+r'\d{2}'+sep+r'\d{4}'
+    date_match = re.search(date_pattern, date_string)
+    return date_match.group() if date_match else None
+
+def get_all_receipts(dir: str = 'receipts', file_prefix: str='item-details', sep: str='-'):
+    receipt_files = [f for f in os.listdir(dir) if f.startswith(file_prefix)]
+    receipts: Dict[str, List[ReceiptItem]] = {}
+    for receipt_file in receipt_files:
+        receipt_date = find_date(receipt_file, sep)
+        if receipt_date is None:
+            print("Could not find date for receipt ", receipt_file)
+            continue
+        receipt = create_receipt(os.path.join(dir,receipt_file))
+        if receipt_date in receipts.keys():
+            receipts[receipt_date].append(receipt)
+        else:
+            receipts[receipt_date] = receipt
+    return receipts
 
 if __name__ == '__main__':
-    create_receipt('receipts/item-details-01-10-2025.json')
+    receipts = get_all_receipts()
+    
